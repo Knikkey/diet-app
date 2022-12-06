45aa7d76c0af495f8000bbce1bb50eedea662859
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-06 10:19:45 +0900

    setup guard page for tdee section

--- src/pages/tdee/Tdee.js
+++ src/pages/tdee/Tdee.js
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { ValuesContext } from "../../context/ValuesContext";
 import TdeeText from "./TdeeText";
 import TdeeCalculator from "./TdeeCalculator";
 import Divider from "../../components/divider/Divider";
@@ -5,10 +7,26 @@ import Divider from "../../components/divider/Divider";
 import styles from "./styles/Tdee.module.css";
 
 export default function Tdee({ id }) {
+  const { bmrState } = useContext(ValuesContext);
+
   return (
-    <div id={id} className={styles.tdee}>
-      <TdeeText />
-      <TdeeCalculator />
+    <div id={id} className={bmrState ? styles.tdee : styles["tdee--wait"]}>
+      {bmrState && (
+        <>
+          <TdeeText />
+          <TdeeCalculator />
+        </>
+      )}
+      {!bmrState && (
+        <div className={styles["wait-container"]}>
+          <p className={styles.wait}>Wait!</p>
+          <p className={styles["wait-text"]}>
+            We know you're excited to excited to learn, but let's take it
+            slowly. Let's figure out your <a href="#bmr">BMR</a> first before we
+            continue.
+          </p>
+        </div>
+      )}
       <Divider color="deep-green" />
     </div>
   );
