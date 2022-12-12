41697a84a9a22302e8dc08ff2eba2314180c6845
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-12 09:28:21 +0900

    removed sliderjs

--- src/pages/basics/Basics.js
+++ src/pages/basics/Basics.js
@@ -1,12 +1,12 @@
 import Divider from "../../components/divider/Divider";
-import BasicsSwiper from "./BasicsSwiper";
+import Slide1 from "./Slide1";
 
 import styles from "./styles/Basics.module.css";
 
 export default function Basics({ id }) {
   return (
     <div id={id} className={styles.basics}>
-      <BasicsSwiper />
+      <Slide1 />
       <Divider color="emerald" />
     </div>
   );
