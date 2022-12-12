7c74a5d081438af0426f3a4456d1e2bbab344d5b
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-12 10:58:51 +0900

    view slide2

--- src/pages/basics/Basics.js
+++ src/pages/basics/Basics.js
@@ -1,12 +1,13 @@
 import Divider from "../../components/divider/Divider";
 import Slide1 from "./Slide1";
+import Slide2 from "./Slide2";
 
 import styles from "./styles/Basics.module.css";
 
 export default function Basics({ id }) {
   return (
     <div id={id} className={styles.basics}>
-      <Slide1 />
+      <Slide2 />
       <Divider color="emerald" />
     </div>
   );
