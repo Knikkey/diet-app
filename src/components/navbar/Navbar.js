be677570008c9d566d42d668c5ed13746a591e51
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-12 15:25:54 +0900

    navbar shape fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -27,19 +27,6 @@ export default function Navbar() {
 
   return (
     <nav className={`${styles.nav} ${!show && styles["nav__hide"]}`}>
-      <div className={styles.bubble}>
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
-            className={styles["shape-fill"]}
-          ></path>
-        </svg>
-      </div>
       <ul>
         {links.map((link, i) => (
           <li key={i}>
